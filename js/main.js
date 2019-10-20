da74bdf655fb3469c5a6f1b1d05af1fc3ec92789
Ezio <assassinste@gmail.com>
2019-10-20 16:21:24 +0800

    Adding logic to decide the machanism for loading images

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function loadImage(id, targetId) {
+  var el = document.getElementById(id);
+  var targetEl = targetId ? document.getElementById(targetId) : el;
+  var imageToLoad;
+
+  if (el.dataset.image) {
+    // use inline `data-image` to download the image with HTML (1 req instead of multiple)
+    imageToLoad = el.dataset.image;
+  } else if (typeof el.currentSrc === "undefined") {
+    // the browsers doesn't support the `data-X`, use `(img)src` as fallback
+    imageToLoad = el.src;
+  } else {
+    // use the one provided by CSS (at least in our case e.g. `wallpaper` in our 'main.css')
+    imageToLoad = el.currentSrc;
+  }
+
+  if (imageToLoad) {
+    var img = new Image();
+    img.src = imageToLoad;
+    img.onload = function() {
+      targetEl.classList.add("is-loaded");
+    };
+  }
+}
